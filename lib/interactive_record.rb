3739c25690fc71a423f85a31eb1166b9753a8196
Andrew Williams <1williamsaj@gmail.com>
2019-11-24 01:45:27 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
     column = row.keys[0].to_s
     value = row.values[0]
 
-    DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{column} = ?", value);
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{column} = ?", value);
   end
 
 end
