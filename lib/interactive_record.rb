eee27cc5c85f071196db09020496e259e8f568a8
Andrew Williams <1williamsaj@gmail.com>
2019-11-24 01:41:23 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -62,7 +62,8 @@ class InteractiveRecord
       WHERE #{column_name} = ?
     SQL
 
-    DB[:conn].execute(sql, value_name);
+    DB[:conn].execute("SELECT * FROM #{table_name}
+    WHERE #{column_name} = ?", value_name);
   end
 
 end
